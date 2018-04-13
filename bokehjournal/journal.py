e05057ee213be5decb464f90c60b38bb1345154e
Ryan Hausen <ryan.hausen@gmail.com>
2018-04-13 16:09:31 -0700

    Added BR tag and and bokeh plot centering

--- bokehjournal/journal.py
+++ bokehjournal/journal.py
@@ -2,20 +2,38 @@ import webbrowser
 
 import dominate
 import bokeh_utils as bu
-from dominate.tags import link, script, p, h1, h2, h3, ul, li
+from dominate.tags import link, script, p, h1, h2, h3, ul, li, style, br
 from dominate.util import raw
 from bokeh.embed import components
 
 class Journal:
-    def __init__(self, title):
+    def __init__(self, title, css='splendor'):
         self.doc = dominate.document(title=title)
         self.has_widgets = False
         self.has_tables = False
 
         with self.doc.head:
+            link(rel='stylesheet', href=Journal.css[css], type='text/css')
+
+            # bokeh css/js
             link_href, js_src = bu.get_bokeh_src()
             link(rel='stylesheet', href=link_href, type="text/css")
             script(type='text/javascript', src=js_src)
+
+            # css to center the bokeh plots
+            style("""
+            div.bk-grid {
+                margin: auto;
+                width: 50%;
+            }
+
+            div.bk-plot-layout {
+                margin: auto;
+                width: 50%;
+            }
+            """)
+
+
 
 
     def append_paragraph(self, text):
@@ -43,6 +61,11 @@ class Journal:
                     else:
                         li(item)
 
+    def append_br(self, num=1):
+        with self.doc.body:
+            for _ in range(num):
+                br()
+
     def append_bokeh(self, figure):
         js, div = components(figure)
 
@@ -64,6 +87,13 @@ class Journal:
             raise ValueError('Invalid "where" value. Should be {}'.format(new_settings))
 
         webbrowser.open('./output.html', new=new_settings.index(where))
+
+
+    css = {
+        'splendor': "https://rawgit.com/markdowncss/splendor/master/css/splendor.min.css",
+        'retro': "https://rawgit.com//markdowncss/retro/master/css/retro.css",
+        'modest': "https://rawgit.com/markdowncss/air/master/css/splendor.min.css",
+    }
 
 
 
