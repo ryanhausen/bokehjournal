d0d5ef768bb84a4c2ee695c0f33832327bfa9ed9
Ryan Hausen <ryan.hausen@gmail.com>
2018-11-15 14:56:46 -0800

    Updated example and tested with jsdelivr

--- bokehjournal/example.py
+++ bokehjournal/example.py
@@ -1,9 +1,20 @@
 from journal import Journal
 from bokeh.plotting import figure
 
-journal = Journal('Example Output')
+journal = Journal('Example File')
 
-journal.append_h1('This Is An Example!')
+journal.append_h1('Bokeh Journal')
+
+journal.append_paragraph('Bokeh Journal is a tool to create HTML documents' +
+                         'using python and bokeh. It currently supports:')
+
+journal.append_list('h1', 
+                    'h2', 
+                    'h3', 
+                    'ul (this for example)', 
+                    'p', 
+                    'br',
+                    'and Bokeh plots')
 
 x = [1, 2, 3, 4, 5]
 y = [6, 7, 2, 4, 5]
@@ -12,9 +23,4 @@ p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
 p.line(x, y, legend="Temp.", line_width=2)
 journal.append_bokeh(p)
 
-p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
-p.line(x, y, legend="Temp.", line_width=2)
-journal.append_bokeh(p)
-
 journal.show()
-
